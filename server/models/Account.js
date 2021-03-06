007963cc7e4a295ec9f2929dc4bcb9e70d621044
jonesyjava <jonesyjava@gmail.com>
2021-03-05 17:47:41 -0700

    added bio schema

--- server/models/Account.js
+++ server/models/Account.js
@@ -7,7 +7,8 @@ const Account = new Schema(
     _id: { type: String, required: true },
     email: { type: String, lowercase: true, unique: true },
     name: { type: String, required: true },
-    picture: { type: String }
+    picture: { type: String },
+    bio: { type: String, required: true }
     // NOTE If you wish to add additional public properties for Accounts do so here
   },
   { timestamps: true, _id: false, toJSON: { virtuals: true } }
